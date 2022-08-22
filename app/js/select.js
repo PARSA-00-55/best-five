9280fbcf74ac7c2d68701b414197cfc6fbda2652
PARSA-00-55 <parsa02345@gmail.com>
2022-08-22 06:46:27 +0600

    select section js added

---
+++ app/js/select.js
@@ -0,0 +1,80 @@
+//button for lionel messi
+const mainlist = document.getElementById('main-list');
+document.getElementById('btn-one').addEventListener('click', function(){
+
+    function addLitoThelist(){
+        // const stringifiObj = JSON.stringify(livalue);
+        const li = document.createElement('li');
+        li.innerText = 'Lionel Messi';
+        mainlist.appendChild(li);
+    }
+    addLitoThelist()
+},{once : true}
+)
+//button for Neymar JR 
+document.getElementById('btn-two').addEventListener('click', function(){
+
+    function addLitoThelist(){
+        // const mainlist = document.getElementById('main-list');
+        // const stringifiObj = JSON.stringify(livalue);
+        const li = document.createElement('li');
+        li.innerText = 'Neymar Jr';
+        mainlist.appendChild(li);
+    }
+    addLitoThelist()
+},{once : true}
+)
+//button for Kylian Mbappé
+document.getElementById('btn-three').addEventListener('click', function(){
+
+    function addLitoThelist(){
+        // const mainlist = document.getElementById('main-list');
+        // const stringifiObj = JSON.stringify(livalue);
+        const li = document.createElement('li');
+        li.innerText = 'Kylian Mbappé';
+        mainlist.appendChild(li);
+    }
+    addLitoThelist()
+},{once : true}
+)
+//button for Vítor Machado
+document.getElementById('btn-four').addEventListener('click', function(){
+
+    function addLitoThelist(){
+        // const mainlist = document.getElementById('main-list');
+        // const stringifiObj = JSON.stringify(livalue);
+        const li = document.createElement('li');
+        li.innerText = 'Vítor Machado';
+        mainlist.appendChild(li);
+    }
+    addLitoThelist()
+},{once : true}
+)
+//button for Sergio Ramos
+document.getElementById('btn-five').addEventListener('click', function(){
+
+    function addLitoThelist(){
+        // const mainlist = document.getElementById('main-list');
+        // const stringifiObj = JSON.stringify(livalue);
+        const li = document.createElement('li');
+        li.innerText = 'Sergio Ramos';
+        mainlist.appendChild(li);
+    }
+    addLitoThelist()
+},{once : true}
+)
+//button for Renato Sanches
+document.getElementById('btn-six').addEventListener('click', function(){
+    function addLitoThelist(){
+        // const mainlist = document.getElementById('main-list');
+        // const stringifiObj = JSON.stringify(livalue);
+        const li = document.createElement('li');
+        li.innerText = 'Renato Sanches';
+        mainlist.appendChild(li);
+    }
+    addLitoThelist()
+},{once : true}
+)
+
+
+
